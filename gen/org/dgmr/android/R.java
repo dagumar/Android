0821e04b5386ce5787b6c7b93237c1f42d0dc6d5
David Gutiérrez <dagumar@gmail.com>
2012-04-18 09:47:50 +0200

    Añadiendo eventos a un Spinner

--- gen/org/dgmr/android/R.java
+++ gen/org/dgmr/android/R.java
@@ -8,43 +8,46 @@
 package org.dgmr.android;
 
 public final class R {
+    public static final class array {
+        public static final int ciudades=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int blanco=0x7f040002;
-        public static final int negro=0x7f040003;
-        public static final int rojo=0x7f040000;
-        public static final int verde=0x7f040001;
+        public static final int blanco=0x7f050002;
+        public static final int negro=0x7f050003;
+        public static final int rojo=0x7f050000;
+        public static final int verde=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnBorrar=0x7f060006;
-        public static final int btnEnviar=0x7f060007;
-        public static final int button1=0x7f060000;
-        public static final int button2=0x7f06000b;
-        public static final int editTxt=0x7f060005;
-        public static final int editTxtApellidos=0x7f06000a;
-        public static final int editTxtCiudad=0x7f06000c;
-        public static final int editTxtNombre=0x7f060009;
-        public static final int imageView1=0x7f060008;
-        public static final int textView1=0x7f060001;
-        public static final int textView1a=0x7f06000d;
-        public static final int textView1b=0x7f060010;
-        public static final int textView2=0x7f060002;
-        public static final int textView2a=0x7f06000e;
-        public static final int textView2b=0x7f060011;
-        public static final int textView3=0x7f060003;
-        public static final int textView3a=0x7f06000f;
-        public static final int textView3b=0x7f060012;
-        public static final int textView4=0x7f060013;
-        public static final int textView5=0x7f060014;
-        public static final int textView6=0x7f060015;
-        public static final int textView7=0x7f060016;
-        public static final int textView8=0x7f060017;
-        public static final int textView9=0x7f060018;
-        public static final int txtView=0x7f060004;
+        public static final int btnBorrar=0x7f070006;
+        public static final int btnEnviar=0x7f070007;
+        public static final int button1=0x7f070000;
+        public static final int button2=0x7f07000c;
+        public static final int editTxt=0x7f070005;
+        public static final int editTxtApellidos=0x7f07000a;
+        public static final int editTxtNombre=0x7f070009;
+        public static final int imageView1=0x7f070008;
+        public static final int spinCiudad=0x7f07000b;
+        public static final int textView1=0x7f070001;
+        public static final int textView1a=0x7f07000d;
+        public static final int textView1b=0x7f070010;
+        public static final int textView2=0x7f070002;
+        public static final int textView2a=0x7f07000e;
+        public static final int textView2b=0x7f070011;
+        public static final int textView3=0x7f070003;
+        public static final int textView3a=0x7f07000f;
+        public static final int textView3b=0x7f070012;
+        public static final int textView4=0x7f070013;
+        public static final int textView5=0x7f070014;
+        public static final int textView6=0x7f070015;
+        public static final int textView7=0x7f070016;
+        public static final int textView8=0x7f070017;
+        public static final int textView9=0x7f070018;
+        public static final int txtView=0x7f070004;
     }
     public static final class layout {
         public static final int autodestruccion=0x7f030000;
@@ -56,11 +59,11 @@ public final class R {
         public static final int relative=0x7f030006;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int autodestruccion=0x7f050005;
-        public static final int hello=0x7f050000;
-        public static final int primero=0x7f050002;
-        public static final int segundo=0x7f050003;
-        public static final int tercero=0x7f050004;
+        public static final int app_name=0x7f060001;
+        public static final int autodestruccion=0x7f060005;
+        public static final int hello=0x7f060000;
+        public static final int primero=0x7f060002;
+        public static final int segundo=0x7f060003;
+        public static final int tercero=0x7f060004;
     }
 }
--- src/org/dgmr/android/AndroidActivity.java
+++ src/org/dgmr/android/AndroidActivity.java
@@ -5,20 +5,42 @@ import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class AndroidActivity extends Activity {
+public class AndroidActivity extends Activity implements OnItemSelectedListener{
 	TextView text = null;
+
+	EditText editTxtNombre = null;
+	EditText editTxtApellidos = null;
+	Spinner spinCiudad = null;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.formulario_datos_personales);
+		editTxtNombre = ((EditText) findViewById(R.id.editTxtNombre));
+		editTxtApellidos = ((EditText) findViewById(R.id.editTxtApellidos));
+		spinCiudad = ((Spinner) findViewById(R.id.spinCiudad));
+		obtenerValoresSpinner();
+
+
+
 	}
+	public void obtenerValoresSpinner(){
+
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ciudades, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		spinCiudad.setAdapter(adapter);
+	}
+
 
 	public void click(View view) {
 		Button boton = (Button) view;
@@ -31,23 +53,42 @@ public class AndroidActivity extends Activity {
 		linLay.setBackgroundColor(getResources().getColor(R.color.rojo));
 	}
 
-	public void enviar(View view) {
-		((TextView )findViewById(R.id.txtView)).setText( ((EditText) findViewById(R.id.editTxt) ).getText() );
-	}
-
-	public void borrar(View view) {
-		((EditText) findViewById(R.id.editTxt) ).setText(""); 
-	}
-	
 	public void enviarClick(View view) {
-		Log.e("Activity",((EditText) findViewById(R.id.editTxtNombre) ).getText() + "" +  ((EditText) findViewById(R.id.editTxtApellidos) ).getText() + ((EditText) findViewById(R.id.editTxtCiudad) ).getText()  );
+		String nombre = editTxtNombre.getText().toString();
+		String apellidos = editTxtApellidos.getText().toString();
+		String  ciudad = spinCiudad.getSelectedItem().toString();
+		;
+		//String ciudad = editTxtCiudad.getText().toString();
+		Log.e("Activity", nombre + " " + apellidos + " " + ciudad);
 	}
 
 	public void borrarClick(View view) {
+
+		editTxtNombre.setText("");
+		editTxtApellidos.setText("");
+		spinCiudad.setSelection(0);
+		//editTxtCiudad.setText("");
+
+	}
+
+	// Primer formulario simple: coger valor de un edit y rellenar un text view
+	public void enviar(View view) {
+		((TextView) findViewById(R.id.txtView))
+		.setText(editTxtNombre.getText());
+	}
+
+	public void borrar(View view) {
+		((EditText) findViewById(R.id.editTxt)).setText("");
+	}
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		// TODO Auto-generated method stub
 		
-		((EditText) findViewById(R.id.editTxtNombre) ).setText(""); 
-		((EditText) findViewById(R.id.editTxtApellidos) ).setText(""); 
-		((EditText) findViewById(R.id.editTxtCiudad) ).setText("");
+	}
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 
