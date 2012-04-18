434e01317ca2aca9674123d519af085db77fedee
David Gutiérrez <dagumar@gmail.com>
2012-04-18 09:04:38 +0200

    Diversos layouts + formulario complejo

--- gen/org/dgmr/android/R.java
+++ gen/org/dgmr/android/R.java
@@ -10,37 +10,57 @@ package org.dgmr.android;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blanco=0x7f040002;
+        public static final int negro=0x7f040003;
+        public static final int rojo=0x7f040000;
+        public static final int verde=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int textView1=0x7f050000;
-        public static final int textView1a=0x7f050003;
-        public static final int textView1b=0x7f050006;
-        public static final int textView2=0x7f050001;
-        public static final int textView2a=0x7f050004;
-        public static final int textView2b=0x7f050007;
-        public static final int textView3=0x7f050002;
-        public static final int textView3a=0x7f050005;
-        public static final int textView3b=0x7f050008;
-        public static final int textView4=0x7f050009;
-        public static final int textView5=0x7f05000a;
-        public static final int textView6=0x7f05000b;
-        public static final int textView7=0x7f05000c;
-        public static final int textView8=0x7f05000d;
-        public static final int textView9=0x7f05000e;
+        public static final int btnBorrar=0x7f060006;
+        public static final int btnEnviar=0x7f060007;
+        public static final int button1=0x7f060000;
+        public static final int button2=0x7f06000b;
+        public static final int editTxt=0x7f060005;
+        public static final int editTxtApellidos=0x7f06000a;
+        public static final int editTxtCiudad=0x7f06000c;
+        public static final int editTxtNombre=0x7f060009;
+        public static final int imageView1=0x7f060008;
+        public static final int textView1=0x7f060001;
+        public static final int textView1a=0x7f06000d;
+        public static final int textView1b=0x7f060010;
+        public static final int textView2=0x7f060002;
+        public static final int textView2a=0x7f06000e;
+        public static final int textView2b=0x7f060011;
+        public static final int textView3=0x7f060003;
+        public static final int textView3a=0x7f06000f;
+        public static final int textView3b=0x7f060012;
+        public static final int textView4=0x7f060013;
+        public static final int textView5=0x7f060014;
+        public static final int textView6=0x7f060015;
+        public static final int textView7=0x7f060016;
+        public static final int textView8=0x7f060017;
+        public static final int textView9=0x7f060018;
+        public static final int txtView=0x7f060004;
     }
     public static final class layout {
-        public static final int colorines=0x7f030000;
-        public static final int linear=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int relative=0x7f030003;
+        public static final int autodestruccion=0x7f030000;
+        public static final int colorines=0x7f030001;
+        public static final int formulario=0x7f030002;
+        public static final int formulario_datos_personales=0x7f030003;
+        public static final int linear=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int relative=0x7f030006;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int primero=0x7f040002;
-        public static final int segundo=0x7f040003;
-        public static final int tercero=0x7f040004;
+        public static final int app_name=0x7f050001;
+        public static final int autodestruccion=0x7f050005;
+        public static final int hello=0x7f050000;
+        public static final int primero=0x7f050002;
+        public static final int segundo=0x7f050003;
+        public static final int tercero=0x7f050004;
     }
 }
--- src/org/dgmr/android/AndroidActivity.java
+++ src/org/dgmr/android/AndroidActivity.java
@@ -2,12 +2,53 @@ package org.dgmr.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative);
-    }
+	TextView text = null;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.formulario_datos_personales);
+	}
+
+	public void click(View view) {
+		Button boton = (Button) view;
+		boton.setText("3, 2, 1");
+		text = (TextView) findViewById(R.id.textView1);
+		text.setText("Redrum");
+		Log.e("ACTIVITY", "La app se va a autodestruir");
+		LinearLayout linLay = (LinearLayout) findViewById(R.id.textView1)
+				.getParent();
+		linLay.setBackgroundColor(getResources().getColor(R.color.rojo));
+	}
+
+	public void enviar(View view) {
+		((TextView )findViewById(R.id.txtView)).setText( ((EditText) findViewById(R.id.editTxt) ).getText() );
+	}
+
+	public void borrar(View view) {
+		((EditText) findViewById(R.id.editTxt) ).setText(""); 
+	}
+	
+	public void enviarClick(View view) {
+		Log.e("Activity",((EditText) findViewById(R.id.editTxtNombre) ).getText() + "" +  ((EditText) findViewById(R.id.editTxtApellidos) ).getText() + ((EditText) findViewById(R.id.editTxtCiudad) ).getText()  );
+	}
+
+	public void borrarClick(View view) {
+		
+		((EditText) findViewById(R.id.editTxtNombre) ).setText(""); 
+		((EditText) findViewById(R.id.editTxtApellidos) ).setText(""); 
+		((EditText) findViewById(R.id.editTxtCiudad) ).setText("");
+		
+	}
+
 }
